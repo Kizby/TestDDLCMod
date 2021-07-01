876846e80b867500a95390de5227bff391b2978b
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-07-01 18:40:38 -0400

    Hello World

---
+++ TestDDLCMod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BepInEx;
+
+namespace TestDDLCMod
+{
+    [BepInPlugin("org.kizbyspark.plugins.testddlcmod", "Test DDLC Mod", "0.1.0.0")]
+    [BepInProcess("Doki Doki Literature Club Plus.exe")]
+    public class TestDDLCMod : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Hello, world!");
+        }
+    }
+}
