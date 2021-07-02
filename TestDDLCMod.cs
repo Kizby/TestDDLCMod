c57cee471b9b3e333cd2f85506a21d76cedc5576
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-07-02 14:27:25 -0400

    Rudimentary Mods button inserted into start menu

--- TestDDLCMod.cs
+++ TestDDLCMod.cs
@@ -1,5 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
 using BepInEx;
+using HarmonyLib;
+using RenpyLauncher;
+using UnityEngine;
 
 namespace TestDDLCMod
 {
@@ -9,7 +16,79 @@ namespace TestDDLCMod
     {
         void Awake()
         {
-            UnityEngine.Debug.Log("Hello, world!");
+            Harmony harmony = new Harmony("org.kizbyspark.plugins.testddlcmod");
+            harmony.PatchAll();
         }
     }
+    
+    [HarmonyPatch(typeof(DesktopApp), "Start")]
+    public class PatchDesktopStart
+    {
+        static void Prefix(DesktopApp __instance) {
+            Debug.Log("In Prefix");
+            var StartMenuContainer = __instance.DesktopDesktop.transform.Find("StartMenuContainer") as RectTransform;
+            StartMenuContainer.sizeDelta += new Vector2(0, 73);
+            var StartMenuItemCanvas = __instance.DesktopDesktop.transform.Find("StartMenuItemCanvas") as RectTransform;
+            var QuitButton = StartMenuItemCanvas.Find("QuitButton");
+            var ModsButton = UnityEngine.Object.Instantiate(QuitButton);
+            QuitButton.localPosition -= new Vector3(0, 73);
+            ModsButton.SetParent(StartMenuItemCanvas, false);
+            var ModsTextComponent = ModsButton.Find("QuitButtonText (TMP)").GetComponent<TMPro.TextMeshProUGUI>();
+            ModsTextComponent.text = "Mods";
+            __instance.ButtonStrings.Insert(__instance.ButtonStrings.Count - 1, ModsTextComponent.text);
+            __instance.ButtonTexts.Insert(__instance.ButtonTexts.Count - 1, ModsTextComponent);
+        }
+    }
+    [HarmonyPatch()]
+    public static class PatchDesktopStartMenuToggle
+    {
+        static Predicate<CodeInstruction> StartPredicate = instruction => instruction.Is(OpCodes.Call, AccessTools.Method(typeof(RenpyParser.Utils), "IsConsolePlatform"));
+        static Predicate<CodeInstruction> EndPredicate = instruction => instruction.opcode == OpCodes.Stloc_3;
+
+        static MethodBase TargetMethod()
+        {
+            Type DesktopApp = typeof(DesktopApp);
+            foreach (Type NestedType in DesktopApp.GetNestedTypes(BindingFlags.NonPublic)) {
+                if (NestedType.Name.Contains("StartMenuToggle"))
+                {
+                    BindingFlags MethodFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+                    return NestedType.GetMethod("MoveNext", MethodFlags);
+                }
+            }
+            return null;
+        }
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            bool Replacing = false;
+            foreach (var instruction in instructions)
+            {
+                if (StartPredicate(instruction))
+                {
+                    Replacing = true;
+                    yield return new CodeInstruction(OpCodes.Ldloc_1);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PatchDesktopStartMenuToggle), "GetStartMenuHeight"));
+                }
+                if (Replacing)
+                {
+                    Replacing = !EndPredicate(instruction);
+                }
+                if (!Replacing)
+                {
+                    yield return instruction;
+                }
+            }
+        }
+        
+        static float GetStartMenuHeight(DesktopApp app)
+        {
+            Debug.Log("In GetStartMenuHeight");
+            var ButtonCount = app.ButtonTexts.Count;
+            if (RenpyParser.Utils.IsConsolePlatform())
+            {
+                --ButtonCount;
+            }
+            return 69 + 74 * ButtonCount;
+        }
+    }
+    //*/
 }
