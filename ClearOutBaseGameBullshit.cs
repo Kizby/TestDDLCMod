0925a67b9b8bc3498b0acfffdba1501cde2a4585
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-07-05 12:47:11 -0400

    Why does the base game call unimplemented methods ;-;

---
+++ ClearOutBaseGameBullshit.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace TestDDLCMod
+{
+    [HarmonyPatch(typeof(DDLCMain_ProxyLib), "init_python_startuppythonblock_1820771261")]
+    class IDGAFIfMethodNotImplemented
+    {
+        static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
