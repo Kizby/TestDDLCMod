3f3213279376fa64b17c0dc257b1598dbf386694
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-07-07 10:46:07 -0400

    Checkpoint work walking the AST

--- RPYCFile.cs
+++ RPYCFile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 
@@ -7,6 +8,9 @@ namespace TestDDLCMod
     public class RPYCFile
     {
         public bool Ok { get; private set; } = false;
+        public Dictionary<string, PythonObj> Labels = new Dictionary<string, PythonObj>();
+        public List<PythonObj> Inits = new List<PythonObj>();
+        public Dictionary<string, Dictionary<string, PythonObj>> Stores = new Dictionary<string, Dictionary<string, PythonObj>>();
 
         public RPYCFile(byte[] fileBytes)
         {
@@ -17,14 +21,27 @@ namespace TestDDLCMod
                 return;
             }
 
-            var beforeStatic = GetSlotAST(fileBytes, magic.Length, 1);
-            Debug.Log("BeforeStatic pickled type is: " + beforeStatic.Type);
-            var afterStatic = GetSlotAST(fileBytes, magic.Length + 12, 2);
-            Debug.Log("AfterStatic pickled type is: " + afterStatic.Type);
+            // try to get statements after static transforms
+            var slotAST = GetSlotAST(fileBytes, magic.Length, 2);
+            if (null == slotAST)
+            {
+                // if they're not there, grab the ones before static transforms
+                slotAST = GetSlotAST(fileBytes, magic.Length, 1);
+            }
+
+            InitHandlers();
+
+            var stmts = slotAST.Tuple[1];
+            Debug.Log(stmts);
+            foreach (var stmt in stmts.List)
+            {
+                HandleStatement(stmt);
+            }
         }
 
         PythonObj GetSlotAST(byte[] bytes, int offset, int slot)
         {
+            offset += (slot - 1) * 12;
             if (slot != BitConverter.ToInt32(bytes, offset))
             {
                 Debug.LogError("Wrong slot in rpyc?");
@@ -32,8 +49,102 @@ namespace TestDDLCMod
             }
             var start = BitConverter.ToInt32(bytes, offset + 4);
             var length = BitConverter.ToInt32(bytes, offset + 8);
+            if (length == 0)
+            {
+                return null;
+            }
 
             return Unpickler.UnpickleZlibBytes(bytes, start, length);
+        }
+
+        private int depth = 0;
+        private void Log(string s)
+        {
+            Debug.Log(new string(' ', depth) + s);
+        }
+
+        private void HandleStatement(PythonObj statement)
+        {
+            var name = statement.Name;
+            if (!handlers.ContainsKey(name))
+            {
+                Log("Unhandled class: " + name);
+            }
+            else
+            {
+                handlers[name](statement);
+            }
+        }
+
+        private void HandleBlock(PythonObj container)
+        {
+            ++depth;
+            var block = container.Fields["block"].List;
+            foreach(var statement in block)
+            {
+                HandleStatement(statement);
+            }
+            --depth;
+        }
+
+
+        Dictionary<string, Action<PythonObj>> handlers = null;
+        private void InitHandlers()
+        {
+            handlers = new Dictionary<string, Action<PythonObj>>()
+            {
+                {
+                    "renpy.ast.Node", stmt =>
+                    {
+                        // name, filename, linenumber, next
+                    }
+                },
+                {
+                    "renpy.ast.Label", stmt =>
+                    {
+                        handlers["renpy.ast.Node"](stmt);
+                        // name, parameters, block, hide
+                        var name = stmt.Fields["name"].String;
+                        Log("label " + name + ":");
+                        Labels[name] = stmt;
+                        HandleBlock(stmt);
+                    }
+                },
+                {
+                    "renpy.ast.Init", stmt =>
+                    {
+                        handlers["renpy.ast.Node"](stmt);
+                        // block, priority
+                        Inits.Add(stmt);
+                        HandleBlock(stmt);
+                    }
+                },
+                {
+                    "renpy.ast.Translate", stmt =>
+                    {
+                        handlers["renpy.ast.Node"](stmt);
+                        // identifier, language, block
+                        Log("translate");
+                        HandleBlock(stmt);
+                    }
+                },
+                {
+                    "renpy.ast.Define", stmt =>
+                    {
+                        handlers["renpy.ast.Node"](stmt);
+                        // varname, code, store
+                        var store = stmt.Fields["store"].String;
+                        if (!Stores.ContainsKey(store))
+                        {
+                            Stores.Add(store, new Dictionary<string, PythonObj>());
+                        }
+                        var name = stmt.Fields["varname"].String;
+                        var code = stmt.Fields["code"];
+                        Log("Defining " + store + ": " + name);
+                        Stores[store][name] = code;
+                    }
+                },
+            };
         }
     }
 }
